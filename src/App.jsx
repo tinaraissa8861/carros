a4becfd2a1108df3a8ae214156d3780b393f3de8
tinaraissa88 <raissafabriciolima3@gmail.comm>
2024-03-18 18:34:29 -0300

    quarto commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,23 @@ export default function App() {
       <section>
         <div className="bg-orange">
           <img src={iconSedans}lt="icone dos carros sedan" />
-          <h2>Sedans</h2>
-          <p>Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
-          <a href="https://centercarjf.com.br/blog/detalhe/10568/carros-sedan-12-modelos-em-alta-no-mercado-de-seminovos" target="_blank">Ver mais</a>
+          <h2 className="font-shoulders">Sedans</h2>
+          <p className="font-lexend">Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
+          <a  className="font-lexend" href="https://centercarjf.com.br/blog/detalhe/10568/carros-sedan-12-modelos-em-alta-no-mercado-de-seminovos" target="_blank">Ver mais</a>
         </div>
 
         <div className="bg-green">
         <img src={iconSuvs}alt="icone dos carros Suvs" />
-          <h2>Suvs</h2>
-          <p>Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
-          <a href="https://autopapo.uol.com.br/noticia/suvs-mais-baratos-brasil-2024/" target="_blank">Ver mais</a>
+          <h2 className="font-shoulders">Suvs</h2>
+          <p  className="font-lexend">Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
+          <a  className="font-lexend" href="https://autopapo.uol.com.br/noticia/suvs-mais-baratos-brasil-2024/" target="_blank">Ver mais</a>
         </div>
 
         <div className="bg-dark-green">
         <img src={iconLuxury}alt="icone dos carros Luxury" />
-          <h2>Luxury</h2>
-          <p>Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
-          <a href="https://www.luxurymotorsport.com.br/" target="_blank">Ver mais</a>
+          <h2 className="font-shoulders">Luxury</h2>
+          <p  className="font-lexend">Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
+          <a  className="font-lexend" href="https://www.luxurymotorsport.com.br/" target="_blank">Ver mais</a>
         </div>
           
         
