71ab2229180c240a5767221d501a3c242a57abeb
tinaraissa88 <raissafabriciolima3@gmail.comm>
2024-03-25 14:58:18 -0300

    segundo e ultimo commit da responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,25 @@ import iconLuxury from "./assets/icon-luxury.svg"
 export default function App() {
   return(
     <main className=" min-h-svh bg-offwhitebg py-20 px-6 antialiased lg:flex justfy-center items-center">
-      <section className=" lg:flex ">
-        <div className="bg-orange p-12 rounded-t-lg">
+      <section className=" lg:flex max-w{920px}">
+        <div className="bg-orange p-12 max-lg:rounded-t-lg lg:rounded-l-lg">
           <img src={iconSedans} alt="icone dos carros sedan" className="mb-7" />
           <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg">Sedans</h2>
-          <p className="font-lexend mb-7 text-offwhite">Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
-          <a  className="font-lexend bg-offwhitebg block px-10 py-3 w-fit rounded-3xl text-orange" href="https://centercarjf.com.br/blog/detalhe/10568/carros-sedan-12-modelos-em-alta-no-mercado-de-seminovos" target="_blank">Ver mais</a>
+          <p className="font-lexend mb-7 text-offwhite lg:mb20">Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
+          <a  className="font-lexend bg-offwhitebg block px-10 py-3 w-fit rounded-3xl text-orange border-2 border-offwhitebg hover:bg-orange hover:text-offwhitebg" href="https://centercarjf.com.br/blog/detalhe/10568/carros-sedan-12-modelos-em-alta-no-mercado-de-seminovos" target="_blank">Ver mais</a>
         </div>
-
         <div className="bg-green p-12">
         <img src={iconSuvs}alt="icone dos carros Suvs" className="mb-7"/>
           <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg">Suvs</h2>
           <p  className="font-lexend mb-7 text-offwhite">Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
-          <a  className="font-lexend bg-offwhitebg block px-10 py-3 w-fit rounded-3xl text-green" href="https://autopapo.uol.com.br/noticia/suvs-mais-baratos-brasil-2024/" target="_blank">Ver mais</a>
+          <a  className="font-lexend bg-offwhitebg block px-10 py-3 w-fit rounded-3xl text-green border-2 border-offwhitebg hover:bg-green hover:text-offwhitebg" href="https://autopapo.uol.com.br/noticia/suvs-mais-baratos-brasil-2024/" target="_blank">Ver mais</a>
         </div>
 
-        <div className="bg-dark-green p-12 rounded-b-lg">
+        <div className="bg-dark-green p-12 max-lg:rounded-b-lg lg:rounded-r-lg">
         <img src={iconLuxury}alt="icone dos carros Luxury"className="mb-7" />
           <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg">Luxury</h2>
           <p  className="font-lexend mb-7 text-offwhite">Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
-          <a  className="font-lexend bg-offwhitebg block px-10 py-3 w-fit rounded-3xl text-darck-green" href="https://www.luxurymotorsport.com.br/" target="_blank">Ver mais</a>
+          <a  className="font-lexend bg-offwhitebg block px-10 py-3 w-fit rounded-3xl text-darck-green border-2 border-offwhitebg hover:bg-dark-green hover:text-offwhitebg" href="https://www.luxurymotorsport.com.br/" target="_blank">Ver mais</a>
         </div>
           
         
