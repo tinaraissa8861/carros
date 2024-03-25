d0eb8910c79302832d4b9bce9ba15491aa4caa88
tinaraissa88 <raissafabriciolima3@gmail.comm>
2024-03-25 16:04:06 -0300

    projeto finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import Luxuryy from "./componentes/Luxury"
 export default function App() {
   return(
     <main className=" min-h-svh bg-offwhitebg py-20 px-6 antialiased lg:flex justfy-center items-center">
-      <section className=" lg:flex max-w{920px}">
+      <div className=" lg:flex max-w{920px}">
         <Sedans />
         <Suvs />
         <Luxuryy />
-      </section>
+      </div>
     </main>
   )
 }
